441b6132ab9d8b1226e764c9db795ec0c6b787f8
Mazaaj <johanelmazaa@gmail.com>
2024-10-30 21:14:40 -0400

    primer commit

---
+++ data.py
@@ -0,0 +1,15 @@
+question_data = [
+    {"text": "La Tierra es el único planeta en el sistema solar con vida..", "answer": "Falso"},
+    {"text": "El agua hierve a 100 grados Celsius a nivel del mar.", "answer": "Verdad"},
+    {"text": "El corazón humano tiene cuatro cavidades.", "answer": "Falso"},
+    {"text": "El sistema solar tiene ocho planetas.", "answer": "Verdad"},
+    {"text": "El Everest es la montaña más alta del mundo.", "answer": "Falso"},
+    {"text": "¿El director Quentin Tarantino es conocido por sus películas con finales ambiguos"
+             " y abiertos a la interpretación?.", "answer": "Verdad"},
+    {"text": "¿La película 'El silencio de los corderos' está basada en una historia real?", "answer": "Falso"},
+    {"text": "¿El idioma inglés es el idioma más hablado del mundo?.", "answer": "Falso"},
+    {"text": "¿La Antártida es un continente?.", "answer": "Verdad"},
+    {"text": "¿El corazón humano late alrededor de 100.000 veces al día?'.", "answer": "Verdad"},
+    {"text": "¿El átomo es la unidad más pequeña de un elemento que puede existir?.", "answer": "Falso"},
+    {"text": "¿Es cierto que la Luna tiene su propia luz?.", "answer": "Falso"}
+]
